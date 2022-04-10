701ef477dd5a38c87f2c2bded0977aa29a78f4aa
Miguel Guthridge <hdsq@outlook.com.au>
2022-04-10 15:10:36 +1000

    Improve formatting

--- tests/test_signatures/my_module.py
+++ tests/test_signatures/my_module.py
@@ -1,6 +1,13 @@
+"""
+tests > test_signatures > my_module
+
+Example code used to help with testing item signatures
+"""
+
 
 def function():
     ...
+
 
 class Class:
     def method(self):
--- tests/test_versions/test_versions.py
+++ tests/test_versions/test_versions.py
@@ -1,26 +1,31 @@
 
 from verva.version import VersionSpec
 
+
 def test_any():
     s = VersionSpec(min_version=None, max_version=None)
     assert 1 in s
+
 
 def test_min():
     s = VersionSpec(min_version=1, max_version=None)
     assert 2 in s
     assert 0 not in s
 
+
 def test_max():
-    s =  VersionSpec(min_version=None, max_version=10)
+    s = VersionSpec(min_version=None, max_version=10)
     assert 9 in s
     assert 10 not in s
 
+
 def test_longer_min():
-    s = VersionSpec(min_version=(1,0,1), max_version=None)
+    s = VersionSpec(min_version=(1, 0, 1), max_version=None)
     assert 1 not in s
-    assert (1,0,1) in s
+    assert (1, 0, 1) in s
+
 
 def test_longer_max():
-    s = VersionSpec(min_version=None, max_version=(1,0,1))
+    s = VersionSpec(min_version=None, max_version=(1, 0, 1))
     assert 1 in s
-    assert (1,0,1) not in s
+    assert (1, 0, 1) not in s
