1e834f948ca13676be19ea1545afdb507746007e
Miguel Guthridge <hdsq@outlook.com.au>
2022-04-11 13:28:09 +1000

    Rename tests

--- tests/signature_test/signature_test.py
+++ tests/signature_test/signature_test.py
@@ -7,19 +7,19 @@ from . import my_module
 def test_function_signatures():
     assert (
         getSignature(my_module.function)
-        == "tests.test_signatures.my_module.function"
+        == "tests.signature_test.my_module.function"
     )
 
 
 def test_class_signatures():
     assert (
         getSignature(my_module.Class)
-        == "tests.test_signatures.my_module.Class"
+        == "tests.signature_test.my_module.Class"
     )
 
 
 def test_method_signatures():
     assert (
         getSignature(my_module.Class.method)
-        == "tests.test_signatures.my_module.Class.method"
+        == "tests.signature_test.my_module.Class.method"
     )
